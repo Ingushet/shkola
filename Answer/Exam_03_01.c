9b7631f65046aecfca69b6e6d94d5cb2abd27bac
asadas <asadas@gmail.com>
2024-05-04 22:08:51 +0300

    Exam_03_01.c

--- Answer/Exam_03_01.c
+++ Answer/Exam_03_01.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include <math.h>
-int main (void)
-{
+#include <stdio.h>
+int main(void) {
     double result, V, R, P = 21500;
-    if (scanf("%lf", &R) != 1 || R <= 0 ){
+    if (scanf("%lf", &R) != 1 || R <= 0) {
         printf("n/a");
+    } else {
+        V = (4.0 / 3.0) * pow(R, 3) * M_PI;
+        double m = P * V;
+        result = round(m);
+        printf("%.0lf", result);
     }
-    else {
-    V =(4.0/3.0) * pow(R, 3) * M_PI;
-    double m = P * V;
-    result = round (m);
-	printf("%.0lf", result);}
-	return 0;
+    return 0;
 }
