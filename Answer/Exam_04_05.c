b26a5db8c24b2c01da907f087dc4fbb48f62557d
asadas <asadas@gmail.com>
2024-05-06 08:54:19 +0300

    -A

--- Answer/Exam_04_05.c
+++ Answer/Exam_04_05.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
-int number(int num);
 int main() {
-    int num;
+    int num, res = 1, flag = 0;
     if (scanf("%d", &num) != 1) {
         printf("n/a");
-    }
-
-    else {
-        printf("%d", number(num));
+    } else {
+        if (num < 0) {
+            num = -num;
+        }
+        while (num > 0) {
+            int temp = num % 10;
+            if (temp % 2 != 0) {
+                res *= temp;
+                flag = 1;
+            }
+            num /= 10;
+        }
+        if (flag) {
+            printf("%d", res);
+        } else {
+            printf("0");
+        }
     }
     return 0;
-}
-
-int number(int num) {
-    int res = 1;
-    if (num < 0) {
-        num = -num;
-    }
-    while (num > 0) {
-        int temp = num % 10;
-        if (temp % 2 != 0) {
-            res *= temp;
-        }
-        num /= 10;
-        if (res == 1 && temp % 2 == 0) {
-            return 0;
-        }
-    }
-    return res;
 }
