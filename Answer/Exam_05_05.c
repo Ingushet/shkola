b9bbfcb4ed7f34354afc6d43f42f616f1f7c6ade
asadas <asadas@gmail.com>
2024-05-09 10:17:51 +0300

    Exam_05_05.c

--- Answer/Exam_05_05.c
+++ Answer/Exam_05_05.c
@@ -1,39 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-    int c = 0, num, mass = 2, tmax, tmin, temp;
-    int *data = malloc(sizeof(int) * mass);
-    while (scanf("%d", &num) == 1 && num != -1) {
-        if (c == mass - 1) {
-            mass *= 2;
-            int *data2 = malloc(sizeof(int) * mass);
-            for (int i = 0; i < c; i++) {
-                data2[i] = data[i];
-                *data = *data2;
-            }
-        }
-        data[c] = num;
-        c++;
+    int c = 0, num, mass[1000];
+    while(scanf("%d", &num) == 1 && num != -1){
+        if(c != 0){
+        if(num == mass[c - 1]){
+        c++;}
+        else{
+         mass[c] = num;
+         c++;
+        }}
+        else {
+        mass[c] = num;
+        c++;}
     }
-    tmax = tmin = 0;
-    for (int i = 0; i < c; i++) {
-        if (data[i] < data[tmin]) {
-            tmin = i;
-        }
-        if (data[i] > data[tmax]) {
-            tmax = i;
-        }
+    for(int i = 0; i < c; i++){
+        printf("%d", mass[i]);
     }
-    temp = data[tmin];
-    data[tmin] = data[tmax];
-    data[tmax] = temp;
-
-    for (int i = 0; i < c; i++) {
-        printf("%d", data[i]);
-        if (i != c - 1) {
-            printf(" ");
-        }
-    }
-    free(data);
     return 0;
 }
