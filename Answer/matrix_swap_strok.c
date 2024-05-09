fa6682d2bbf6a7858e31ad087cb678c3376b4b0e
asadas <asadas@gmail.com>
2024-05-09 23:40:10 +0300

    matrix_swap_strok.c

---
+++ Answer/matrix_swap_strok.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n, m;
+    if(scanf("%d %d", &n, &m) != 2 || n < 1 || m < 1){
+        printf("n/a");
+    }
+    else{
+    int **matrix = (int**)malloc(sizeof(int*) * n);
+    int *ptr = (int*)malloc(sizeof(int)* n * m);
+    for(int i = 0; i < n; i++){
+        matrix[i] = ptr + i * m;
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    int temp = 0;
+    for(int j = 0; j < m; j++){
+        temp = matrix[0][j];
+        matrix[0][j] = matrix[n-1][j];
+        matrix[n-1][j] = temp;
+    }
+
+    for(int i = 0; i < n; i++){
+        if(i != 0){printf("\n");}
+        for(int j = 0; j < m; j++){
+        printf("%d", matrix[i][j]);
+        if(j != m - 1){printf(" ");}
+    }}
+    }
+    return 0;
+
+}
