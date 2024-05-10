cb88be18753bacc378ecd02eeaccdf44245e5141
asadas <asadas@gmail.com>
2024-05-10 08:25:11 +0300

    Exam_05_05.c

--- Answer/Exam_05_05.c
+++ Answer/Exam_05_05.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 int main() {
-    int c = 0, num, mass[1000];
-    while(scanf("%d", &num) == 1 && num != -1){
-        if(c != 0){
-        if(num == mass[c - 1]){
-        c++;}
-        else{
-         mass[c] = num;
-         c++;
-        }}
-        else {
+    int num, mass[1000], c = 0;
+
+    while (scanf("%d", &num) == 1 && num != -1) {
         mass[c] = num;
-        c++;}
+        c++;
     }
-    for(int i = 0; i < c; i++){
-        printf("%d", mass[i]);
+
+    for (int i = 0; i < c; i++) {
+        int flag = 0;
+        for (int j = 0; j < i; j++) {
+            if (mass[j] == mass[i]) {
+                flag = 1;
+                break;
+            }
+        }
+        if (!flag) {
+            if (i != 0) {
+                printf(" ");
+            }
+            printf("%d", mass[i]);
+        }
     }
+
     return 0;
 }
